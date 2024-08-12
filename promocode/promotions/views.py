a6eea9179dd1dde2619778760e1cdb6129ca1503
Wagner Palmeira <wagnerpalmeira.ti@gmail.com>
2024-08-12 20:18:11 -0300

    Adding templates

--- promocode/promotions/views.py
+++ promocode/promotions/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
 
+promotions = [
+    {
+        'title': 'Post Um',
+        'price': 29.99,
+        'url': 'https://google.com'
+    },
+    {
+        'title': 'Robo Aspirador Kabum Smart 900',
+        'price': 129.99,
+        'url': 'https://www.kabum.com.br/produto/366169/robo-aspirador-kabum-smart-900-127v-branco-ir360-controle-via-app-recipiente-dispensador-de-po-kbsf011'
+    },
+]
+
 def promotions_list(request):
-    return render(request, 'promotions/index.html')
+    context  = {
+        'promotions': promotions,
+    }
+    return render(request, 'promotions/index.html', context)
