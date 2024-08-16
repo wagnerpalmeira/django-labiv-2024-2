a4fb62c57c9eeba4a315f9815fe7ee9e21c69bf7
Wagner Palmeira <wagnerpalmeira.ti@gmail.com>
2024-08-16 18:51:48 -0300

    Intro templates

--- promocode/promocode/settings.py
+++ promocode/promocode/settings.py
@@ -113,12 +113,18 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
+]
+
+# Criar ENV: python -m venv env
+# ATIVAR ENV: ./env/Scripts/activate
+# INSTALL DJANGO: pip install django
--- promocode/promocode/urls.py
+++ promocode/promocode/urls.py
@@ -2,8 +2,15 @@
 from django.contrib import admin
 from django.urls import path
 from promotions.views import promotions_list
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('promotions/', promotions_list),
     # path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT)
