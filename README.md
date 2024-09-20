c4ef8e9b3690727b83ae890360d4370ef6f557d8
Wagner Palmeira <wagnerpalmeira.ti@gmail.com>
2024-09-20 18:57:19 -0300

    Adding migrations

---
+++ README.md
@@ -0,0 +1,11 @@
+## Criar env
+
+python -m venv env
+
+## Ativas env
+1. No terminal digitar:
+- ./env/Scripts/activate
+
+## Instalar Dependencias
+
+pip install django
--- promocode/promotions/models.py
+++ promocode/promotions/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+user_model = get_user_model()
+
+class Promotion(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    url = models.URLField() 
+
+    user = models.ForeignKey(user_model, on_delete=models.PROTECT)
+
+class Comment(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+
+    user = models.ForeignKey(user_model, on_delete=models.PROTECT)
+    promotion = models.ForeignKey(Promotion, on_delete=models.PROTECT)
